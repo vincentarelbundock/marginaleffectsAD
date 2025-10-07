01b34ceb07a9c1b55bbd973f5f1b7a3c341f75f8
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-10-07 07:47:52 -0400

    cruft

--- marginaleffectsAD/__init__.py
+++ marginaleffectsAD/__init__.py
@@ -4,10 +4,6 @@
 from . import linear as linear
 from . import glm as glm
 
-# Import high-level summary functions
-from .summary import predictions as predictions
-from .summary import comparisons as comparisons
-
 # Re-export types for convenience
 from .comparisons import ComparisonType as ComparisonType
 from .glm.families import Family as Family, Link as Link
